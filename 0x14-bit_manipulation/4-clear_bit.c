05f438f19af91e77b02a09eeaeda631fd5223b35
Bright Kusi Appiah <appiahkusibright@gmail.com>
2023-04-09 08:17:32 -0700

    Additional Task Completed

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,18 +1,19 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * clear_bit - A function that sets a bit to 0 at given index
- * @n: The number to set bit in
- * @index: The index to set bit at
+ * clear_bit - A function that sets a bit to 0 at given an index
+ * Script was written by Briht Kusi Appiah
+ * @n: The number to set the bit in
+ * @index: The index to set the bit at
  * Return: 1 if it worked, or -1 on error
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int max = 0x01;
-
-	max = ~(max << index);
-	if (max == 0x00)
+	/* Declaring the if conditionality */
+	if (index > 63)
 		return (-1);
-	*n &= max;
+
+	/* End part of code */
+	*n = (~(1UL << index) & *n);
 	return (1);
 }
