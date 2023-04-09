c1b46d3fa1fdbf9d2a503f5ddd5df94f12502537
Bright Kusi Appiah <appiahkusibright@gmail.com>
2023-04-09 07:16:04 -0700

    Additional Task Completed

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,31 +1,30 @@
 #include "main.h"
 
 /**
- * print_binary - A function that prints a binary number 
- * This has to be done without % or /
+ * print_binary - A function that prints a binary number without % or /
  * This script was written by Bright Kusi Appiah
- * @n: The number to be printed
- * Return: Nothing
+ * @n: The number set to be printed
+ * Return: Nothing. This function returns nothing.
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int me = 0, me1 = 32768; 
-	/* 1000 0000 0000 0000 */
+	int me1, me_read = 0;
+	/* Declare variables */
+	unsigned long int me_present;
+	/* More variables above */
 
-	if (n == 0)
+	for (me1 = 63; me1 >= 0; me1--)
 	{
-		_putchar('0');
-		return;
-	}
-	while (me1)
-	{
-		if (me == 1 && (n & me1) == 0)
-			_putchar('0');
-		else if ((n & me1) != 0)
+		me_present = n >> me1;
+
+		if (me_present & 1)
 		{
 			_putchar('1');
-			me = 1;
+			me_read++;
 		}
-		me1 >>= 1;
+		else if (me_read)
+			_putchar('0');
 	}
+	if (!me_read)
+		_putchar('0');
 }
