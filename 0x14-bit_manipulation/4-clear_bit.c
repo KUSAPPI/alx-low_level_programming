26a9a445b2a18df1b8365b046c07bd432be4b476
Bright Kusi Appiah <appiahkusibright@gmail.com>
2023-04-09 08:20:24 -0700

    Mandatory Task Completed

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,8 +1,8 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * clear_bit - A function that sets a bit to 0 at given an index
- * Script was written by Briht Kusi Appiah
+ * Script was written by Bright Kusi Appiah
  * @n: The number to set the bit in
  * @index: The index to set the bit at
  * Return: 1 if it worked, or -1 on error
