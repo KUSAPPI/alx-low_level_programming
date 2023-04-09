44b3b8e496bf40ec03f660c9b5a89a2d6d2200e4
Bright Kusi Appiah <appiahkusibright@gmail.com>
2023-04-09 06:29:34 -0700

    Mandatory Task Completed

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * set_bit - A function that sets a bit at given index to 1
+ * This script was written by Bright Kusi Appiah
+ * @n: The number to set the bit in
+ * @index: The index to set the bit at
+ * Return: 1 if it worked, or -1 on error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int me = 0x01;
+	/* Variables declared here */
+
+	me <<= index;
+	if (me == 0)
+		return (-1);
+
+	*n |= me;
+	return (1);
+}
+
