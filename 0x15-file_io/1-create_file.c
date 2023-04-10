9110206a1aa3ecd17bffcc4932a586984b9fc628
Bright Kusi Appiah <appiahkusibright@gmail.com>
2023-04-10 13:49:54 -0700

    Task Completed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,29 +1,30 @@
 #include "main.h"
 
 /**
- * create_file - A function that creates a file
- * @filename: The filename to create
- * @text_content: A NULL terminated string to write to the file
- * Return: 1 on success, -1 if file can not be created, nor written,
- * nor write fails.
+ * create_file - A function that creates a new file
+ * This file is for educational purposes
+ * @filename: The name of the file to create
+ * @text_content: A NULL terminated pointer string to write to the file
+ * Return: 1 on success, -1 if file, cannot be created, or written to,
+ * or the write fails.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fdo, fdw, len = 0;
+        int me, me1, len = 0;
 
-	if (filename == NULL)
-		return (-1);
+        if (filename == NULL)
+                return (-1);
 
-	fdo = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (fdo < 0)
-		return (-1);
+        me = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+        if (me < 0)
+                return (-1);
 
-	while (text_content && *(text_content + len))
-		len++;
+        while (text_content && *(text_content + len))
+                len++;
 
-	fdw = write(fdo, text_content, len);
-	close(fdo);
-	if (fdw < 0)
-		return (-1);
-	return (1);
+        me1 = write(me, text_content, len);
+        close(me);
+        if (me1 < 0)
+                return (-1);
+        return (1);
 }
