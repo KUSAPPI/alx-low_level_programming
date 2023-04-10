88c5193ea72587360dbcd080aedcb3bff85a565e
Bright Kusi Appiah <appiahkusibright@gmail.com>
2023-04-10 13:29:23 -0700

    Task Completed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,18 +1,12 @@
 #include "main.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/uio.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <stdlib.h>
 
 /**
- * read_textfile - A function that reads any text file, and prints
- * to the POSIX STDOUT as required
- * This script is for educational purposes (ALX SE)
- * @filename: The filename to open, as required
- * @letters: The number of letters to read, and print as required
- * Return: The number of letters read and printed, print 0 on failure
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
