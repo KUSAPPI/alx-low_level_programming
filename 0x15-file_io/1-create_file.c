f69a528a0d56f43988122c4858dab890b2ddd479
Bright Kusi Appiah <appiahkusibright@gmail.com>
2023-04-10 13:55:21 -0700

    Task Completed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,21 +10,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-        int me, me1, len = 0;
+	int me, me1, len = 0;
 
-        if (filename == NULL)
-                return (-1);
+	if (filename == NULL)
+		return (-1);
 
-        me = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-        if (me < 0)
-                return (-1);
+	me = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (me < 0)
+		return (-1);
 
-        while (text_content && *(text_content + len))
-                len++;
+	while (text_content && *(text_content + len))
+		len++;
 
-        me1 = write(me, text_content, len);
-        close(me);
-        if (me1 < 0)
-                return (-1);
-        return (1);
+	me1 = write(me, text_content, len);
+	close(me);
+	if (me1 < 0)
+		return (-1);
+	return (1);
 }
