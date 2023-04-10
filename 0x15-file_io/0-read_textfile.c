bf2e451d1a265ada4742b949b7392088176b1a24
Bright Kusi Appiah <appiahkusibright@gmail.com>
2023-04-10 13:38:53 -0700

    Task Completed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,27 +2,29 @@
 #include <stdlib.h>
 
 /**
- * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
- *        0 when function fails or filename is NULL.
+ * read_textfile- This function reada text file
+ * and prints it to the POSIX standard output.
+ * This script is for educational purposes.
+ * @filename: The text file that is being read
+ * @letters: The number of letters that are to be read
+ * Return: The actual number of bytes that are read and printed
+ * Prints 0 when the function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	char *me;
+	ssize_t me2;
+	ssize_t me3;
+	ssize_t me4;
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	me2 = open(filename, O_RDONLY);
+	if (me2 == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
+	me = malloc(sizeof(char) * letters);
+	me4 = read(me2, me, letters);
+	me3 = write(STDOUT_FILENO, me, me4);
 
-	free(buf);
-	close(fd);
-	return (w);
+	free(me);
+	close(me2);
+	return (me3);
 }
